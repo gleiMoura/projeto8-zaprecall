75a8443e6fc5526d3c5b209055fdd97bf113d777
Gleison Moura <gle.bar.mou@gmail.com>
2022-03-20 19:54:42 -0300

    put the second screen with the cards. Now is possible choose and answer the question. I the end is possible choose one option from three to remenber after the result of the recall

--- src/index.js
+++ src/index.js
@@ -1,14 +1,51 @@
 import ReactDOM from 'react-dom';
-import Start from './startScreen'
+import React from 'react';
+import Question from './question'
 
-function App(){
-    return(
+const dice = [
+    { number: "Pergunta 1", content: "O que é JSX" },
+];
+
+function Start() {
+    const [selected, setSelected] = React.useState(true);
+    const state_page_two = `page page-two ${selected ? "hide" : ""}`;
+    const state_page_one = `page page-one ${selected ? "" : "hide"}`
+    return (
+        <>
+            <div className={state_page_one}>
+                <div className="logo-start">
+                    <img src="img/logo.png" alt="logo do jogo" />
+                    <h1>ZapReacall</h1>
+                </div>
+                <button className="button-start" onClick={() => setSelected(!selected)}>Iniciar Recall!</button>
+            </div>
+
+            <main className={state_page_two}>
+                <div className="logo-question">
+                    <img src="img/logo.png" alt="logo do jogo" />
+                    <h1>ZapReacall</h1>
+                </div>
+                <section className = "questions">
+                    {dice.map(data => <Question number_question={data.number} content_question={data.content} />)}
+                </section>
+            </main>
+
+            <footer>
+        
+            </footer>
+        </>
+    )
+}
+
+
+
+function App() {
+    return (
         <>
             <Start />
         </>
     )
 }
-
 const divRoot = document.querySelector('.root')
 
-ReactDOM.render(<App/>, divRoot)
+ReactDOM.render(<App />, divRoot)
